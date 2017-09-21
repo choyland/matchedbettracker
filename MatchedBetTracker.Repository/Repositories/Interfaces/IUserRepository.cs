b940b5ce5e245f2925f26317436dc09776903203
Charlie Hoyland <charlie.hoyland@asandk.com>
2017-09-21 16:09:25 +0100

    basic repository and model structure added

---
+++ MatchedBetTracker.Repository/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using MatchedBetTracker.Model.Entities;
+
+namespace MatchedBetTracker.Repository.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        string StatusMessage { get; set; }
+        Task CreateUser(User user);
+        Task<User> GetUser();
+    }
+}
